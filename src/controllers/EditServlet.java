a69dd7619c8366990039c4ce9885b9b46958506e
kai.yasuda <kai0603603@gmail.com>
2020-07-31 10:12:58 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -41,8 +41,10 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("tasklist", t);
         request.setAttribute("_token", request.getSession().getId());
 
-
-        request.getSession().setAttribute("tasklist_id", t.getId());
+        if(t != null){
+            
+            request.getSession().setAttribute("tasklist_id", t.getId());
+        }
 
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/edit.jsp");
